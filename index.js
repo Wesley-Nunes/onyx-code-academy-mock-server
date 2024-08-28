1e09b4003f164996080f3327bdfe5138e8fda28d
Wesley-Nunes <wesnmonteiro@gmail.com>
2024-08-28 19:13:33 -0300

    read the env variables

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import multer from 'multer';
 import cors from 'cors';
+import 'dotenv/config';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
