eb0e51d44c1c0ad7de35a0057a866500ff81432f
Wes <wesnmonteiro@gmail.com>
2024-08-29 17:30:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import 'dotenv/config';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
